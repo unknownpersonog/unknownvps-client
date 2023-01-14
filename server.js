8b567ed27be680b6a7e7f5da324b5e238fef9845
unknownpersonog <104310243+unknownpersonog@users.noreply.github.com>
2023-01-14 14:44:56 +0000

    dash test 10

--- server.js
+++ server.js
@@ -97,7 +97,6 @@ app.get('/login',async(req,res)=>{
         console.log('Error',error)
         return res.send('Some error occurred! ')
     } 
-  })
     app.get('/dashboard',(req,res)=>{
       res.send(`
 <html lang="en"><head>
@@ -169,3 +168,4 @@ html, body {
         `)   
     }
 )
+  })
