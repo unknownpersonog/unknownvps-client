8f263e7f1822128acd9acc3d61b039d102d982f8
unknownpersonog <104310243+unknownpersonog@users.noreply.github.com>
2023-01-22 15:57:21 +0530

    DB Implementation Complete | Coins System Part#1 (#3)
    * Trigger cyclic
    
    * db test #1
    
    * fix#1.1
    
    * cyclic trigger
    
    * DB Implementaion Part#1
    
    * DB Implementation | Coins System [BETA]
    
    * Theme now matches to login screen
    
    * Unneeded css fixed
    
    Co-authored-by: cyclic-sh[bot] <84505167+cyclic-sh[bot]@users.noreply.github.com>

--- server.js
+++ server.js
@@ -52,13 +52,59 @@ function ensureAuthenticated(req, res, next) {
 
 app.get('/login', passport.authenticate('discord'));
 
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: 'sql12.freemysqlhosting.net',
+    user: 'sql12592296',
+    password: 'Y6pEGJgbsY',
+    database: 'sql12592296'
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to MySQL database');
+});
+
+
+// Function to check if user already exists in database
+function checkUserExists(userId, callback) {
+  connection.query("SELECT * FROM users WHERE discord_id = ?", [userId], function (err, result) {
+    if (err) throw err;
+    callback(result);
+  });
+}
+
 app.get('/login/callback', 
   passport.authenticate('discord', { failureRedirect: '/login' }),
   function(req, res) {
+    checkUserExists(req.user.id, (result) => {
+        if(result.length > 0) {
+            // update user's information
+            const sql = `UPDATE users SET username = '${req.user.username}', email = '${req.user.email}' WHERE discord_id = '${req.user.id}'`;
+            connection.query(sql, (err, result) => {
+                if (err) throw err;
+                console.log('User information updated');
+            });
+        } else {
+            // insert new user's information
+            const sql = `INSERT INTO users (discord_id, username, email, coins) VALUES ('${req.user.id}', '${req.user.username}', '${req.user.email}', 0)`;
+            connection.query(sql, (err, result) => {
+                if (err) throw err;
+                console.log('User information inserted into database');
+            });
+        }
+    });
     // Successful authentication, redirect home.
     res.redirect('/dashboard');
   });
   app.get('/dashboard', ensureAuthenticated, function(req, res) {
     const avatarURL = `https://cdn.discordapp.com/avatars/${req.user.id}/${req.user.avatar}.png`;
-    res.render('dashboard', {user: req.user, avatarURL});
+    connection.query(`SELECT coins FROM users WHERE discord_id = ${req.user.id}`, (err, results) => {
+        if(err) {
+          console.log(err);
+          return res.send('An error occurred');
+        }
+    res.render('dashboard', {user: req.user, avatarURL, coins: results[0].coins});
         })
+    });
