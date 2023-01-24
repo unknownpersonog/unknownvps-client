55b7ee45542ac1bb605e903865b3b8b81fa04fe6
unknownpersonog <104310243+unknownpersonog@users.noreply.github.com>
2023-01-24 13:46:41 +0000

    Dashboard Revamp

--- server.js
+++ server.js
@@ -58,7 +58,8 @@ const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    connectionLimit: 10
 });
 
 connection.connect((err) => {
