7d080eaa238b42f9f8dd67758d23799ab9306c34
unknownpersonog <104310243+unknownpersonog@users.noreply.github.com>
2023-02-03 18:07:39 +0530

    Dashboard Update! ✨ | Includes More (#10)
    Dashboard Update! ✨ Includes More (#9)
    
    This is a very big dashboard update. Might be a "bit" unstable. Create issues if you find any.
    Changes Include:-
    1] Dash Revamp:- Removed the old "no use" dashboard
    2] Now includes .env.example containing example .env
    3] DB Service change:- Now we use MariaDB Atlas instead of MySQL.
    4] DB connection is done before app.listen now ensuring no errors [Untested]
    5] All assests are currently remote this will become local by next update.
    Thats it. Our 100th Commit

--- server.js
+++ server.js
@@ -1,117 +1,165 @@
-const express=require('express');
-const axios=require('axios');
-const fs=require('fs');
-const session = require('express-session');
-const app=express();
-const ejs=require('ejs');
-require('dotenv').config()
-const passport = require('passport');
-const DiscordStrategy = require('passport-discord').Strategy;
+const express = require("express");
+const session = require("express-session");
+const app = express();
+const ejs = require("ejs");
+require("dotenv").config();
+const passport = require("passport");
+const DiscordStrategy = require("passport-discord").Strategy;
 
-app.set('view engine', 'ejs');
-app.get('/',(req,res)=>{
-    res.render('login');
-})
+app.set("view engine", "ejs");
+app.get("/", (req, res) => {
+  res.render("login");
+});
 
-app.use(session({
-    secret: 'your-secret-key',
+app.use(
+  session({
+    secret: "your-secret-key",
     resave: false,
-    saveUninitialized: true
-}));
+    saveUninitialized: true,
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
-passport.use(new DiscordStrategy({
-    clientID: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: 'https://client.unknownnodes.ml/login/callback',
-    scope: ['identify','email','guilds']
-  },
-  function(accessToken, refreshToken, profile, done) {
-    done(null, profile);
-  }
-));
+passport.use(
+  new DiscordStrategy(
+    {
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      callbackURL: process.env.CLIENT_REDIRECT_URI,
+      scope: ["identify", "email", "guilds"],
+    },
+    function (accessToken, refreshToken, profile, done) {
+      done(null, profile);
+    }
+  )
+);
 
-passport.serializeUser(function(user, done) {
-    done(null, user);
+passport.serializeUser(function (user, done) {
+  done(null, user);
 });
 
-passport.deserializeUser(function(user, done) {
-    done(null, user);
+passport.deserializeUser(function (user, done) {
+  done(null, user);
 });
 
 function ensureAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-        return next();
-    }
-    res.redirect('/login');
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
 }
 
-app.get('/login', passport.authenticate('discord'));
+app.get("/login", passport.authenticate("discord"));
 
-const mysql = require('mysql2');
+const mongodb = require("mongodb-legacy");
+const MongoClient = mongodb.MongoClient;
 
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    connectionLimit: 10
-});
+const uri = process.env.DB_URI;
 
-connection.connect((err) => {
-    if (err) throw err;
-    console.log('Connected to MySQL database');
-});
+MongoClient.connect(uri, { useNewUrlParser: true }, (err, client) => {
+  if (err) throw err;
+  console.log("Connected to MongoDB Atlas database");
+  const db = client.db("test");
+  const usersCollection = db.collection("users");
 
-// Function to check if user already exists in database
-function checkUserExists(userId, callback) {
-  connection.query("SELECT * FROM users WHERE discord_id = ?", [userId], function (err, result) {
-    if (err) throw err;
-    callback(result);
-  });
-}
-
-app.get('/login/callback', 
-  passport.authenticate('discord', { failureRedirect: '/login' }),
-  function(req, res) {
-    checkUserExists(req.user.id, (result) => {
-        if(result.length > 0) {
-            // update user's information
-            const sql = `UPDATE users SET username = '${req.user.username}', email = '${req.user.email}' WHERE discord_id = '${req.user.id}'`;
-            connection.query(sql, (err, result) => {
-                if (err) throw err;
-                console.log('User information updated');
-            });
+  app.get(
+    "/login/callback",
+    passport.authenticate("discord", { failureRedirect: "/login" }),
+    function (req, res) {
+      usersCollection.findOne({ discord_id: req.user.id }, (err, user) => {
+        if (err) throw err;
+        if (user) {
+          // update user's information
+          usersCollection.updateOne(
+            { discord_id: req.user.id },
+            { $set: { username: req.user.username, email: req.user.email } },
+            (err, result) => {
+              if (err) throw err;
+              console.log("User information updated");
+            }
+          );
         } else {
-            // insert new user's information
-            const sql = `INSERT INTO users (discord_id, username, email, coins) VALUES ('${req.user.id}', '${req.user.username}', '${req.user.email}', 0)`;
-            connection.query(sql, (err, result) => {
-                if (err) throw err;
-                console.log('User information inserted into database');
-            });
+          // insert new user's information
+          usersCollection.insertOne(
+            {
+              discord_id: req.user.id,
+              username: req.user.username,
+              email: req.user.email,
+              coins: 0,
+            },
+            (err, result) => {
+              if (err) throw err;
+              console.log("User information inserted into database");
+            }
+          );
         }
-    });
-    // Successful authentication, redirect home.
-    res.redirect('/dashboard');
-  });
-  app.get('/dashboard', ensureAuthenticated, function(req, res) {
+      });
+      // Successful authentication, redirect home.
+      res.redirect("/dashboard");
+    }
+  );
+  app.get("/dashboard", ensureAuthenticated, function (req, res) {
     const avatarURL = `https://cdn.discordapp.com/avatars/${req.user.id}/${req.user.avatar}.png`;
-    connection.query(`SELECT coins FROM users WHERE discord_id = ${req.user.id}`, (err, results) => {
-        if(err) {
+    client
+      .db("test")
+      .collection("users")
+      .findOne({ discord_id: req.user.id }, (err, result) => {
+        if (err) {
           console.log(err);
-          return res.send('An error occurred');
+          return res.send("An error occurred");
         }
-    res.render('dashboard', {user: req.user, avatarURL, coins: results[0].coins});
-        })
-    });
-    app.get('/logout', function(req, res, next){
-        req.logout(function(err) {
-          if (err) { return next(err); }
-          res.redirect('/');
+        res.render("dashboard", {
+          user: req.user,
+          avatarURL,
+          coins: result.coins,
         });
       });
-app.listen(3000, ()=>{
-  console.log(`App started on port 3000`);
-})
-      
+  });
+  app.get("/logout", function (req, res, next) {
+    req.logout(function (err) {
+      if (err) {
+        return next(err);
+      }
+      res.redirect("/");
+    });
+  });
+  app.get("/earn", ensureAuthenticated, function (req, res) {
+    const avatarURL = `https://cdn.discordapp.com/avatars/${req.user.id}/${req.user.avatar}.png`;
+    client
+      .db("test")
+      .collection("users")
+      .findOne({ discord_id: req.user.id }, (err, result) => {
+        if (err) {
+          console.log(err);
+          return res.send("An error occurred");
+        }
+        res.render("earn", {
+          user: req.user,
+          avatarURL,
+          coins: result.coins,
+        });
+      });
+  });
+  app.get("/store", ensureAuthenticated, function (req, res) {
+    const avatarURL = `https://cdn.discordapp.com/avatars/${req.user.id}/${req.user.avatar}.png`;
+    client
+      .db("test")
+      .collection("users")
+      .findOne({ discord_id: req.user.id }, (err, result) => {
+        if (err) {
+          console.log(err);
+          return res.send("An error occurred");
+        }
+        res.render("store", {
+          user: req.user,
+          avatarURL,
+          coins: result.coins,
+        });
+      });
+  });
+  
+  app.listen(3000, () => {
+    console.log(`App started on port 3000`);
+  });
+});
