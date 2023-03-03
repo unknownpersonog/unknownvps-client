ddb78ebe3fcaf83dd34f84956792d769dabc616c
unknownpersonog <104310243+unknownpersonog@users.noreply.github.com>
2023-03-03 22:57:16 +0530

    Update setupvps.sh

--- setupvps.sh
+++ setupvps.sh
@@ -1,6 +1,8 @@
 hostnamectl set-hostname UnknownVPS
-passwd root unknownvps
+echo -e "unknownvps\nunknownvps" | passwd root
 apt update
 apt install wget openssh-server -y
 rm -rf /etc/ssh/sshd_config
 wget -O /etc/ssh/sshd_config https://raw.githubusercontent.com/dxomg/sshd_config/main/sshd_config
+systemctl restart ssh
+systemctl restart sshd
